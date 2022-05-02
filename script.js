11243d069d47733e9e1fd55ce6b172cff84d2c51
Cbannerman <cbannerman21@gmail.com>
2022-05-02 17:55:04 -0400

    started code for generating password

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Assignment code here
+
+//writing functions to produce uppercase, lowercase, special characters, and numbers for the pasword generator.
+//using math round to round to the nearest whole number instead of math floor to make randomization more effective.
 function getRandomLowercaseLetter() {
     var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     return lowerCase[Math.round(Math.random() * lowerCase.length)];
@@ -9,16 +12,16 @@ function getRandomUpperCaseLetter() {
     return upperCase[Math.round(Math.random() * upperCase.length)];
 }
 
-function getSpecialCharacter() {
+function getRandomSpecialCharacter() {
     var specChar = ['!', '@', '#', '$', '%', '^', '&', '*', '=', '+', '?'];
     return specChar[Math.round(Math.random() * specChar.length)];
 }
 
-function getNumber() {
+function getRandomNumber() {
     var number = ['1','2', '3', '4', '5', '6', '7', '8', '9', '0'];
     return number[Math.round(Math.random() * number.length)];
 }
-
+// added variables for the confirmation messages when creating the password.
 var upperCaseConfirm = function() {
     var c = confirm("If you'd like to use uppercase letters press ok.")
     if (c == true) {
@@ -58,6 +61,47 @@ var specCharConfirm = function() {
         }
     }
 
+    function generatePassword() {
+        var length = getPasswordLength();
+        var lowerCon = lowerCaseConfirm();
+        var upperCon = upperCaseConfirm();
+        var specCharCon = specCharConfirm();
+        var numberCon = numberConfirm();
+        let password = '';
+        if (lowerCon == false && upperCon == false && numberCon == false && specCharCon == false) {
+            return password = "You must select one type of character, please select again.";
+        }
+        else {
+            while (password.length < length) {
+                var getType = Math.round(Math.random() * 6)
+                switch (getType) {
+                    case 1:
+                        if (lowerCon == true) {
+                            password += getRandomLowercaseLetter();
+                        }
+                        break;
+                    case 2:
+                        if (upperCon == true) {
+                            password += getRandomUppercaseLetter();
+                        } 
+                        break;   
+                    case 3:
+                        if (specCharCon == true) {
+                            password += getRandomSpecialCharacter();
+                        }    
+                        break;
+                    case 4:
+                        if (numberCon == true) {
+                            password += getRandomNumber();
+                        }
+                        break;
+                    default:
+                        console.log("error");
+                        break;
+                }
+            }
+        }
+    }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
